98a3dd5047256ddb9282d8240e25fb2254b3743e
Daniil <sov123in@mail.ru>
2023-08-15 21:29:35 +0400

    file git

---
+++ main.py
@@ -0,0 +1,53 @@
+# name = "Daniil"
+# print("Hello,", name)
+# age = 23
+# # print(age)
+# a = 5
+# print(a)
+# print(id(a))
+# a = "hello"
+# print(a)
+# print(id(a))
+
+# a = d = c = 1
+# print(a,d,c)
+# a,d,c="Hello", 5,9.2
+# print(a,d,c)
+#
+# PI=3.14
+# print(PI)
+# PI=2
+# print(PI)
+# a = 1
+# b = 2
+# print("a:",a)
+# print("b:",b)
+# c = a
+# a = b
+# b = c
+# print(" строка \
+#       символов")
+# print('строка')
+# print(" строка строка строка строка строка строка строка строка строка строка строка строка строка строка строка строка "
+#       "строка строка строка строка строка строка строка строка ")
+# print("D:\\folder\\file.py\"file\"")
+# s1 = "Hello"
+# s2 = "World___"
+# s3  = s1 +" "+ s2
+# print(s3 * 5)
+
+# print(53648715678342567825349925)
+# print(5.3648715678342567825349925)
+
+# print(6 + 2)
+# print(6 - 2)
+# print(6 * 2)
+# print(6 / 2)
+# print(6 / 4)
+# print(6 // 4)
+# print(6 ** 2)
+# print(6 % 4)
+
+num = 4321
+a = num % 10
+print( 'Hello git')
